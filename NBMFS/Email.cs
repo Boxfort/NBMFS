571f59263e742eac5afcfe4c9c1e9a1b779b22f4
Jack Anderson <40208539@live.napier.ac.uk>
2016-10-24 13:41:21 +0100

    added constructor and unimplemented methods

--- NBMFS/Email.cs
+++ NBMFS/Email.cs
@@ -8,5 +8,21 @@ namespace NBMFS
 {
     class Email : Message
     {
+        public Email(string messageID, String body) : base(messageID, body) { }
+
+        public override void processMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void setMessageText(string messageText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void setSender(string sender)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
