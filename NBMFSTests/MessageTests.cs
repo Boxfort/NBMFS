a9064c278ff634343691927f13f8e9ca167e7e3e
Jack Anderson <40208539@live.napier.ac.uk>
2016-10-24 16:44:23 +0100

    basic tests

--- NBMFSTests/MessageTests.cs
+++ NBMFSTests/MessageTests.cs
@@ -1,14 +1,45 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using NBMFS;
+
 namespace NBMFSTests
 {
     [TestClass]
     public class MessageTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CreateTweet_WithValidID()
         {
+            //arrange
+            string messageID = "T012345678";
+            string body = "Testing message body";
+
+            try
+            {
+                Tweet tweet = new Tweet(messageID, body);
+            }
+            catch(ArgumentException ex)
+            {
+                Assert.Fail("Exception thrown :" + ex.Message);
+            }
+
+            //assert
+            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateTweet_WithInvalidID()
+        {
+            //arrange
+            string messageID = "Invalid";
+            string body = "Testing message body";
+
+            Tweet tweet = new Tweet(messageID, body);
+
+            //assert
+            Assert.Fail("No exception thrown");
         }
     }
 }
