d9158bd639e69f23c5620f85a1c11bc538ea42e9
Jack Anderson <40208539@live.napier.ac.uk>
2016-10-24 16:56:07 +0100

    Added SMS class

---
+++ NBMFS/SMS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBMFS
+{
+    class SMS : Message
+    {
+        public SMS(string messageID, string body, int messageTextLength, string senderRegex) : base(messageID, body, 140, @"^\+[0-9]{11,13}")
+        {
+        }
+
+        public override void processMessage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
