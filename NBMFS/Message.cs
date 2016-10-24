a4e3a266ec2fbe91a5de8c4a952e9b213a9034f3
Jack Anderson <40208539@live.napier.ac.uk>
2016-10-24 13:09:27 +0100

    getters and setters for message class

--- NBMFS/Message.cs
+++ NBMFS/Message.cs
@@ -8,6 +8,55 @@ namespace NBMFS
 {
     public abstract class Message
     {
+        private string _messageID;
+        private string _messageBody;
+        private string _sender;
+        private string _messageText;
 
+        Message(string messageID, String body)
+        {
+            setMessageID(messageID);
+            setMessageBody(body);
+        }
+
+        #region Getters and Setters
+
+        public string getMessageID()
+        {
+            return _messageID;
+        }
+
+        public void setMessageID(string messageID)
+        {
+            //Regex to check ID validity. [SET][0-9]{9}
+        }
+
+        public string getMessageBody()
+        {
+            return _messageBody;
+        }
+
+        public void setMessageBody(string body)
+        {
+            _messageBody = body;
+        }
+
+        public string getSender()
+        {
+            return _sender;
+        }
+
+        //Senders for each class of message will be of a different format.
+        public abstract void setSender();
+
+        public string getMessageText()
+        {
+            return _messageText;
+        }
+
+        //Message texts for each class of message vary in length
+        public abstract void setMessageText();
+
+        #endregion
     }
 }
