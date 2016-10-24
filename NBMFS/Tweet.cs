6c9e5cf5adc9625db00aa8545e9e3dc1e847ff43
Jack Anderson <40208539@live.napier.ac.uk>
2016-10-24 13:30:24 +0100

    setter for message text

--- NBMFS/Tweet.cs
+++ NBMFS/Tweet.cs
@@ -8,6 +8,8 @@ namespace NBMFS
 {
     class Tweet : Message
     {
+        private const int MESSAGE_TEXT_LENGTH = 128;
+
         Tweet(string messageID, String body) : base(messageID, body) { }
 
         public override void processMessage()
@@ -15,12 +17,15 @@ namespace NBMFS
             throw new NotImplementedException();
         }
 
-        public override void setMessageText()
+        public override void setMessageText(string messageText)
         {
-            throw new NotImplementedException();
+            if (messageText.Length > MESSAGE_TEXT_LENGTH)
+                throw new ArgumentException();
+
+            _messageText = messageText;
         }
 
-        public override void setSender()
+        public override void setSender(string sender)
         {
             throw new NotImplementedException();
         }
