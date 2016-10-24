c25166c98c497721e6990133d2bd74a87ad79de2
Jack Anderson <40208539@live.napier.ac.uk>
2016-10-24 13:24:18 +0100

    created Tweet class

---
+++ NBMFS/Tweet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBMFS
+{
+    class Tweet : Message
+    {
+        Tweet(string messageID, String body) : base(messageID, body) { }
+    }
+}
