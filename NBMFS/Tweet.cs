b9eccaf86df1ad7a7428fdd930fd126aac5cba44
Jack Anderson <40208539@live.napier.ac.uk>
2016-10-24 13:25:13 +0100

    Added unimplemented methods

--- NBMFS/Tweet.cs
+++ NBMFS/Tweet.cs
@@ -9,5 +9,20 @@ namespace NBMFS
     class Tweet : Message
     {
         Tweet(string messageID, String body) : base(messageID, body) { }
+
+        public override void processMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void setMessageText()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void setSender()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
