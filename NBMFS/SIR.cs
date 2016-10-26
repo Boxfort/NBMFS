d12b95b6993fe07df2fa8c105cc69685e08c4bcc
Jack Anderson <jack_120@hotmail.co.uk>
2016-10-26 16:36:32 +0100

    Significant incident report class

---
+++ NBMFS/SIR.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBMFS
+{
+    public class SIR : Email
+    {
+        public SIR(string messageID, string body, string subject) : base(messageID, body, subject)
+        {
+
+        }
+    }
+}
