cb6d5b4ae01256b42abbea85fe63d7911a7b4855
Jack Anderson <jack_120@hotmail.co.uk>
2016-10-26 16:53:37 +0100

    get set incident

--- NBMFS/Incident.cs
+++ NBMFS/Incident.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NBMFS
 {
-    enum Incident
+    public enum Incident
     {
         Theft,
         StaffAttack,
--- NBMFS/SIR.cs
+++ NBMFS/SIR.cs
@@ -8,9 +8,21 @@ namespace NBMFS
 {
     public class SIR : Email
     {
+        private Incident _incident;
+
         public SIR(string messageID, string body, string subject) : base(messageID, body, subject)
         {
 
+        }
+
+        public Incident getIncident()
+        {
+            return _incident;
+        }
+
+        public void setIncident(Incident incident)
+        {
+            _incident = incident;
         }
     }
 }
